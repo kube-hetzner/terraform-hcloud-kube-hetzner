db3876bca4b6acc212a0ecf8c973c64ccd58856e
Dennis Hoppe <github@debian-solutions.de>
2022-01-13 12:36:10 +0100

    Add missing Terraform provider (local)

--- providers.tf
+++ providers.tf
@@ -1,3 +1,5 @@
 provider "hcloud" {
   token = var.hcloud_token
 }
+
+provider "local" {}
--- versions.tf
+++ versions.tf
@@ -4,5 +4,9 @@ terraform {
       source  = "hetznercloud/hcloud"
       version = ">= 1.0.0, < 2.0.0"
     }
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 2.0.0, < 3.0.0"
+    }
   }
 }
