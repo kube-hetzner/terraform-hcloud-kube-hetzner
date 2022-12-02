54cbe5ebf50f288fa8ca93dcc26f5f170eca040c
maaft <sfssf@safasf.de>
2022-12-02 13:06:53 +0100

    fix secrets

--- init.tf
+++ init.tf
@@ -222,14 +222,14 @@ resource "null_resource" "kustomization" {
 
   # Deploy secrets, logging is automatically disabled due to sensitive variables
   provisioner "remote-exec" {
-    inline = [
-      "set -ex",
-      concat(
-        [for s in var.extra_init_secrets : format("%s %s --dry-run=client -o yaml | kubectl apply -f -", "kubectl -n ${s.namespace} create secret generic ${s.name}", join(" ", [for key, value in s.literals : "--from-literal=${key}=${value}"]))],
+    inline = concat(
+      ["set -ex"],
+      [
         "kubectl -n kube-system create secret generic hcloud --from-literal=token=${var.hcloud_token} --from-literal=network=${hcloud_network.k3s.name} --dry-run=client -o yaml | kubectl apply -f -",
         "kubectl -n kube-system create secret generic hcloud-csi --from-literal=token=${var.hcloud_token} --dry-run=client -o yaml | kubectl apply -f -",
-      )
-    ]
+      ],
+      [for s in var.extra_init_secrets : format("%s %s --dry-run=client -o yaml | kubectl apply -f -", "kubectl -n ${s.namespace} create secret generic ${s.name}", join(" ", [for key, value in s.literals : "--from-literal=${key}=${value}"]))],
+    )
   }
 
   # Deploy our post-installation kustomization
