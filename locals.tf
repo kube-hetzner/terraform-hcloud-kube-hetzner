d77a6ded552be80b129aed1c4b24d0a2675d9b1e
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2023-01-24 18:35:20 +0100

    typo

--- locals.tf
+++ locals.tf
@@ -288,7 +288,7 @@ locals {
   kubelet_arg                 = ["cloud-provider=external", "volume-plugin-dir=/var/lib/kubelet/volumeplugins"]
   kube_controller_manager_arg = "flex-volume-plugin-dir=/var/lib/kubelet/volumeplugins"
   flannel_iface               = "eth1"
-  flannel-backend             = var.encrypt_kubernetes_api ? "wireguard-native" : "vxlan"
+  flannel_backend             = var.encrypt_kubernetes_api ? "wireguard-native" : "vxlan"
 
   ingress_controller = var.ingress_controller
 
