297f4a16d496bfaa684bbbcbeb9139501638d3f2
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2023-04-14 16:01:31 +0200

    Fixed comment
    Can't use #! for comments in Terraform, switch to #

--- autoscaler-agents.tf
+++ autoscaler-agents.tf
@@ -9,7 +9,7 @@ locals {
       ssh_key          = local.hcloud_ssh_key_id
       # for now we use the k3s network, as we cannot reference subnet-ids in autoscaler
       ipv4_subnet_id = hcloud_network.k3s.id
-      #! for now we use x86 snapshot, this needs to implement logic to autodetect between x86 and aarch64 based on server type
+      # for now we use x86 snapshot, this needs to implement logic to autodetect between x86 and aarch64 based on server type
       snapshot_id    = data.hcloud_image.microos_x86_snapshot.id
       firewall_id    = hcloud_firewall.k3s.id
       cluster_name   = local.cluster_prefix
