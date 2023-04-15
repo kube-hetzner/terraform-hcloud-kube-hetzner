fc6e5d523d3a0bc91cc660e77fa432790f8f54c5
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2023-04-15 12:09:35 +0200

    Fixed rancher, added example for arm
    https://github.com/kube-hetzner/terraform-hcloud-kube-hetzner/pull/723#issuecomment-1509303303
    
    Disables rancher PSP so that is works on v1.25, added warning to set k3s ver to v1.25 since v1.26 is the default which isn't supported by Rancher currently.
    
    Added arm nodepool example and warning that it's only available in FSN location currently.

--- locals.tf
+++ locals.tf
@@ -474,6 +474,10 @@ additionalArguments:
 hostname: "${var.rancher_hostname != "" ? var.rancher_hostname : var.lb_hostname}"
 replicas: ${length(local.control_plane_nodes)}
 bootstrapPassword: "${length(var.rancher_bootstrap_password) == 0 ? resource.random_password.rancher_bootstrap[0].result : var.rancher_bootstrap_password}"
+global:
+  cattle:
+    psp:
+      enabled: false
   EOT
 
   cert_manager_values = var.cert_manager_values != "" ? var.cert_manager_values : <<EOT
