8fe49804c237c9c4f45ff200482cb103e247ec90
Wessel Valkenburg <wessel@prevue.ch>
2023-05-02 08:58:36 +0000

    fix: coalesce calico version with NA.

--- locals.tf
+++ locals.tf
@@ -289,7 +289,7 @@ locals {
   }
 
   cni_install_resources = {
-    "calico" = ["https://raw.githubusercontent.com/projectcalico/calico/${local.calico_version}/manifests/calico.yaml"]
+    "calico" = ["https://raw.githubusercontent.com/projectcalico/calico/${coalesce(local.calico_version, "NA")}/manifests/calico.yaml"]
     "cilium" = ["cilium.yaml"]
   }
 
