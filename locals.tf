cf42829419cae453f57f063a459f9aecc6ad308e
Wessel Valkenburg <wessel@prevue.ch>
2023-05-02 08:52:01 +0000

    fix: moved hetzner-csi usage logic to a single place.

--- locals.tf
+++ locals.tf
@@ -8,7 +8,7 @@ locals {
   hcloud_ssh_key_id = var.hcloud_ssh_key_id == null ? hcloud_ssh_key.k3s[0].id : var.hcloud_ssh_key_id
 
   ccm_version    = var.hetzner_ccm_version != null ? var.hetzner_ccm_version : data.github_release.hetzner_ccm[0].release_tag
-  csi_version    = (var.hetzner_csi_version != null || var.disable_hetzner_csi) ? var.hetzner_csi_version : data.github_release.hetzner_csi[0].release_tag
+  csi_version    = length(data.github_release.hetzner_csi) == 0 ? var.hetzner_csi_version : data.github_release.hetzner_csi[0].release_tag
   kured_version  = var.kured_version != null ? var.kured_version : data.github_release.kured[0].release_tag
   calico_version = var.calico_version != null ? var.calico_version : data.github_release.calico[0].release_tag
 
