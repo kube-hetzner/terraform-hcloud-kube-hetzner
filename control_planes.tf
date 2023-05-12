4b7e614d8dd8ef8e78a2a1c06297dd1727ef74f1
Michael Karbowiak <michael@karbowiak.dk>
2023-05-12 20:10:57 +0200

    Be more specific in the kube.tf.example about what it takes to enable zram, and make it impossible to enable it if the zram size isn't specified

--- control_planes.tf
+++ control_planes.tf
@@ -173,7 +173,7 @@ WantedBy=multi-user.target
   }
 
   provisioner "remote-exec" {
-    inline = concat(each.value.zram == true ? [
+    inline = concat(each.value.zram == true && each.value.zram_size != null  ? [
       "modprobe zram",
       "echo '${each.value.zram_size}' > /sys/block/zram0/disksize",
       "mkdir -p /etc/modules-load.d",
@@ -186,7 +186,7 @@ WantedBy=multi-user.target
       "setenforce 0",
       "systemctl enable --now zram.service",
       "setenforce 1"
-    ] : [])
+    ] : ["echo 'zram is disabled'"])
   }
 
   depends_on = [
