e1ca719d46255ff8dbb4456d47a2421262229f48
Wessel Valkenburg <wessel@prevue.ch>
2024-01-22 14:03:47 +0000

    feat: flag for enable iscsid regardless of longhorn settings.

--- agents.tf
+++ agents.tf
@@ -105,7 +105,7 @@ resource "null_resource" "agents" {
 
   # Start the k3s agent and wait for it to have started
   provisioner "remote-exec" {
-    inline = concat(var.enable_longhorn ? ["systemctl enable --now iscsid"] : [], [
+    inline = concat(var.enable_longhorn || var.enable_iscsid ? ["systemctl enable --now iscsid"] : [], [
       "systemctl start k3s-agent 2> /dev/null",
       <<-EOT
       timeout 120 bash <<EOF
