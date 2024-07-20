0083a6ed0e4e7a113e9e5f803ac7a6526712de7a
Stephan Sahm <stephan.sahm@jolin.io>
2024-07-20 12:56:39 +0200

    typo

--- locals.tf
+++ locals.tf
@@ -225,8 +225,8 @@ locals {
   # if we are in a single cluster config, we use the default klipper lb instead of Hetzner LB
   control_plane_count    = sum([for v in var.control_plane_nodepools : v.count])
   agent_count            = sum([for v in var.agent_nodepools : length(coalesce(v.nodes, {})) + coalesce(v.count, 0)])
-  autoscaler_count       = sum([for v in var.autoscaler_nodepools : v.max_count])
-  is_single_node_cluster = (local.control_plane_count + local.agent_count + local.autoscaler_count) == 1
+  autoscaler_max_count   = sum([for v in var.autoscaler_nodepools : v.max_nodes])
+  is_single_node_cluster = (local.control_plane_count + local.agent_count + local.autoscaler_max_count) == 1
 
   using_klipper_lb = var.enable_klipper_metal_lb || local.is_single_node_cluster
 
