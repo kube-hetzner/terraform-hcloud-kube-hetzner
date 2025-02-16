1b6b2642eaf6637e0c112fa17807191f391b2e97
Karim Naufal <rimkashox@gmail.com>
2025-02-16 20:49:12 +0100

    Add support for custom kubeconfig server address in control plane configuration
    Enhance TLS SAN configuration to allow specifying a custom kubeconfig server address when using a load balancer

--- control_planes.tf
+++ control_planes.tf
@@ -111,8 +111,9 @@ locals {
     },
     lookup(local.cni_k3s_settings, var.cni_plugin, {}),
     var.use_control_plane_lb ? {
-      tls-san = concat([hcloud_load_balancer.control_plane.*.ipv4[0], hcloud_load_balancer_network.control_plane.*.ip[0]], var.additional_tls_sans)
-      } : {
+      tls-san = concat([
+        hcloud_load_balancer.control_plane.*.ipv4[0],
+        hcloud_load_balancer_network.control_plane.*.ip[0], var.kubeconfig_server_address != "" ? var.kubeconfig_server_address : null], var.additional_tls_sans) : {
       tls-san = concat([
         module.control_planes[k].ipv4_address
       ], var.additional_tls_sans)
