39dc47fd8a3232c3f3d273f9a6e0500a4b51fb91
Marek Pikuła <marek@serenitycode.dev>
2025-06-02 23:19:56 +0000

    Fix external load balancer for agent-less cluster
    It fixes an issue where the external load balancer couldn't be created
    if the cluster is configured in control-plane only configuration (e.g.,
    HA configuration but without agents).
    
    Signed-off-by: Marek Pikuła <marek@serenitycode.dev>

--- init.tf
+++ init.tf
@@ -23,7 +23,11 @@ resource "hcloud_load_balancer_network" "cluster" {
   count = local.has_external_load_balancer ? 0 : 1
 
   load_balancer_id = hcloud_load_balancer.cluster.*.id[0]
-  subnet_id        = hcloud_network_subnet.agent.*.id[0]
+  subnet_id = (
+    length(hcloud_network_subnet.agent) > 0
+    ? hcloud_network_subnet.agent.*.id[0]
+    : hcloud_network_subnet.control_plane.*.id[0]
+  )
 }
 
 resource "hcloud_load_balancer_target" "cluster" {
