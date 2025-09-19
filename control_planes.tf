724528f4076e86012c8fac07728cf252a53a8298
Adonis <adopi.naj@gmail.com>
2025-09-19 21:57:45 +0200

    readable extraction of hostname from url

--- control_planes.tf
+++ control_planes.tf
@@ -95,7 +95,7 @@ resource "hcloud_load_balancer_service" "control_plane" {
 }
 
 locals {
-  control_plane_endpoint_host = var.control_plane_endpoint != null ? trim(element(concat(regex("^https?://(?:[^@/]*@)?(\\[[^\]]+\\]|[^:/]+)", var.control_plane_endpoint), [""]), 0), "[]") : null
+  control_plane_endpoint_host = var.control_plane_endpoint != null ? replace(split(":", split("/", replace(replace(var.control_plane_endpoint, "https://", ""), "http://", ""))[0])[0], ".*@", "") : null
   control_plane_ips = {
     for k, v in module.control_planes : k => coalesce(
       v.ipv4_address,
