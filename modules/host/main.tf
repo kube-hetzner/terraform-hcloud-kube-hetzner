9a7c4d069b73b3bcbfc91b67594f0f30943d6cc8
Edhar Rybak <edgar@rybak.one>
2025-10-19 18:52:05 -0400

    set private_netwrok_ipv4_range for cloutdinit_config

--- modules/host/main.tf
+++ modules/host/main.tf
@@ -188,6 +188,7 @@ data "cloudinit_config" "config" {
         cloudinit_runcmd_common      = var.cloudinit_runcmd_common
         swap_size                    = var.swap_size
         private_network_only         = (var.disable_ipv4 && var.disable_ipv6)
+        private_network_ipv4_range   = "10.207.0.1"
       }
     )
   }
